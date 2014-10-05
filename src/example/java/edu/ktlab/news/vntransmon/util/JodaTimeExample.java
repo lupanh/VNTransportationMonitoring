6aa706c8639fd0ca7a334ffdc5913733a02feae1
lupanh <lupanh@yahoo.com>
2014-10-05 10:40:12 +0700

    Fix timezone

--- src/example/java/edu/ktlab/news/vntransmon/util/JodaTimeExample.java
+++ src/example/java/edu/ktlab/news/vntransmon/util/JodaTimeExample.java
@@ -1,13 +1,17 @@
 package edu.ktlab.news.vntransmon.util;
 
+import java.util.TimeZone;
+
 import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.joda.time.DateTimeZone;
 import org.elasticsearch.common.joda.time.format.DateTimeFormat;
 
 public class JodaTimeExample {
 
 	public static void main(String[] args) {
 		String pattern = "dd/MM/yyyy HH:mm";
-		DateTime dateTime = DateTime.parse("04/10/2014 19:41", DateTimeFormat.forPattern(pattern));
+		DateTime dateTime = DateTime.parse("13/03/2011 02:22", DateTimeFormat.forPattern(pattern))
+				.withZone(DateTimeZone.forTimeZone(TimeZone.getTimeZone("GMT+7")));
 		System.out.println(dateTime.toString());
 	}
 }
--- src/main/java/edu/ktlab/news/vntransmon/util/JodaTimeParser.java
+++ src/main/java/edu/ktlab/news/vntransmon/util/JodaTimeParser.java
@@ -1,12 +1,16 @@
 package edu.ktlab.news.vntransmon.util;
 
+import java.util.TimeZone;
+
 import org.elasticsearch.common.joda.time.DateTime;
+import org.elasticsearch.common.joda.time.DateTimeZone;
 import org.elasticsearch.common.joda.time.format.DateTimeFormat;
 
 public class JodaTimeParser {
 	public static String parseDate(String date) {
 		String pattern = "dd/MM/yyyy HH:mm";
-		DateTime dateTime = DateTime.parse(date, DateTimeFormat.forPattern(pattern));
+		DateTime dateTime = DateTime.parse(date, DateTimeFormat.forPattern(pattern)).withZone(
+				DateTimeZone.forTimeZone(TimeZone.getTimeZone("GMT+7")));
 		return dateTime.toString();
 	}
 }
